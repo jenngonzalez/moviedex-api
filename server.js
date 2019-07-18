cab78c20c5d304d790a0f175b198c12a9fabe9c0
Jenn Gonzalez <jgonz9873@gmail.com>
2019-07-17 18:59:03 -0700

    added search filter functionality and cors security

--- server.js
+++ server.js
@@ -1,18 +1,59 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 const movieStore = require ('./movie-store.json');
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello from the Movie Server!')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    console.log('validate bearer token middleware')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request'})
+    }
+
+    next()
 })
 
-app.get('/movie', (req, res) => {
-    res.json(movieStore)
+
+app.get('/movie', function handleGetMovie(req, res) {
+
+    let response = movieStore
+
+    if(req.query.genre) {
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+            )
+    }
+
+    if(req.query.country) {
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+            )
+    }
+
+    if(req.query.avg_vote) {
+        response = response.filter(movie =>
+            movie.avg_vote >= req.query.avg_vote
+            )
+    }
+
+    if(response.length < 1) {
+        res.send(`Sorry, no films found with those parameters`)
+    }
+
+    res.json(response)
 })
+
 
 const PORT = 8000
 
